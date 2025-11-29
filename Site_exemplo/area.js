5316da3d705fd6775254425c5c3c49d96c27b578
Kalak João <kajopensativo06@gmail.com>
2025-11-29 08:25:32 +0100

    Look
    Modificação no código

---
+++ Site_exemplo/area.js
@@ -0,0 +1,8 @@
+let var1= document.querySelector("p")
+      let a=10;
+      let b=20;
+        if (a**10> b*100){
+              var1.setAttribute('style','Background-color: red; color: blue; border-radius:5px; padding:5px');
+        }else{
+  var1.setAttribute('style','Background-color: black; color: white; border-radius:5px; padding:5px');
+        }
