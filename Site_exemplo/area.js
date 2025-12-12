90601d707e04b86a82f756e8b580f80ca4ba547e
Kalak João <kajopensativo06@gmail.com>
2025-12-12 21:17:31 +0100

    actualização de imagem

--- Site_exemplo/area.js
+++ Site_exemplo/area.js
@@ -1,4 +1,7 @@
 
-const var1 = document.getElementById('Entrar')
-
-
+onmouseover = function(){
+    this.document.getElementById('image').setAttribute("src","kajo2.png")
+}
+onmouseout = function(){
+    this.document.getElementById('image').setAttribute("src","kajo1.jpg")
+}
